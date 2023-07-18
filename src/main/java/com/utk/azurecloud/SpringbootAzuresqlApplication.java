c5b770ff6bfd68e66b18cab17d66d1d7154eaa28
utkarshhingle <utkarshh@gmail.com>
2023-07-18 12:24:43 +0530

    Cross Origin Added

--- src/main/java/com/utk/azurecloud/SpringbootAzuresqlApplication.java
+++ src/main/java/com/utk/azurecloud/SpringbootAzuresqlApplication.java
@@ -3,6 +3,7 @@ package com.utk.azurecloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,6 +44,7 @@ public class SpringbootAzuresqlApplication {
 	}
 	
 	@GetMapping("/employees")
+	@CrossOrigin(origins ="*")
 	public List<EmployeeTest> getEmployees() {
 	    return repository.findAll();
 	}
